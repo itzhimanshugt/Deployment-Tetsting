dad15a274912c255c5bd91d1c572dd22419fb6ad
itzhimanshugt <himanshukvs70@gmail.com>
2025-03-04 10:25:48 +0530

    third commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+
 require('dotenv').config()
 
 const express = require('express');
@@ -9,19 +9,25 @@ const port = process.env.PORT;
 
 app.get('/',(req,res)=>{
     console.log(`Suchabitchassup!`);
+    res.send('Welcome to the landing page!');
+
 });
 
 app.listen(port,() => {
-                console.log(`listening on port ${port}`);
-            });
+    console.log(`listening on port ${port}`);
+});
 
 app.get('/login',(req,res)=>{
     console.log(`you are at login page`);
+    res.send('Welcome to the login page!');
+
 });
 
 
 app.get('/hello',(req,res)=>{
     console.log(`<h1>This is another trial by Admin</h1>`);
+    res.send('Welcome to the hello page!');
+
 });
 
 
